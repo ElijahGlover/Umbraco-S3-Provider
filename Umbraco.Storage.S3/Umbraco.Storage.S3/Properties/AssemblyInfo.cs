630f89a7d9836a196274bbc98ae4b3bf285b9815
Elijah Glover <elijah@elijah.com.au>
2014-06-11 15:00:17 +1000

    NuGet Package Spec

--- Umbraco.Storage.S3/Umbraco.Storage.S3/Properties/AssemblyInfo.cs
+++ Umbraco.Storage.S3/Umbraco.Storage.S3/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Umbraco.Storage.S3")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Umbraco S3 Filesystem Provider")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Elijah Glover")]
 [assembly: AssemblyProduct("Umbraco.Storage.S3")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
