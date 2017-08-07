521c0a6c61c4d9174dc5ff95b7e493be62e1cac3
federicosutin <federico.sutin@nzme.co.nz>
2017-08-07 12:18:29 +1200

    Add Stream Support: Response Code 206 for some extensions

--- Umbraco.Storage.S3/Umbraco.Storage.S3/FileSystemVirtualPathProviderFile.cs
+++ Umbraco.Storage.S3/Umbraco.Storage.S3/FileSystemVirtualPathProviderFile.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
+using System.Linq;
+using System.Web;
 using System.Web.Hosting;
 
 namespace Umbraco.Storage.S3
@@ -7,18 +10,32 @@ namespace Umbraco.Storage.S3
     internal class FileSystemVirtualPathProviderFile : VirtualFile
     {
         private readonly Func<Stream> _stream;
+        private readonly string[] _extensionArray;
 
         public FileSystemVirtualPathProviderFile(string virtualPath, Func<Stream> stream) : base(virtualPath)
         {
             if (stream == null)
                 throw new ArgumentNullException("stream");
             _stream = stream;
+
+            string validExtensions = ConfigurationSettings.AppSettings["Umbraco.S3.Stream.Extensions"];
+            _extensionArray = validExtensions?.Split(',');
         }
 
         public override Stream Open()
         {
+            if (HttpContext.Current != null)
+            {
+                var path = HttpContext.Current.Request.FilePath;
+                if (_extensionArray.Any(x => path.Contains(x)))
+                {
+                    HttpContext.Current.Response.AppendHeader("Accept-Ranges", "bytes");
+                }
+            }
+
             return _stream();
         }
+
 
         public override bool IsDirectory
         {
@@ -26,3 +43,4 @@ namespace Umbraco.Storage.S3
         }
     }
 }
+
